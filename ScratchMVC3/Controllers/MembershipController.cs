18f298fd77fc624e62a42837b59893a04bfee775
Dia Brown <dbrown1903@outlook.com>
2015-04-29 13:43:09 -0600

    All Changes
    In class demo and code along

---
+++ ScratchMVC3/Controllers/MembershipController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScratchMVC3.Controllers
+{
+    public class MembershipController : Controller
+    {
+        // GET: Membership
+        public ActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
